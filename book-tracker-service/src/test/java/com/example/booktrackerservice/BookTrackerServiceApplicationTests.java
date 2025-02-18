18f91c2ff9176142abeb6d34a335bd30b2ca4b45
german <112763670+gerushenka@users.noreply.github.com>
2025-02-18 21:06:14 +0300

    add testcontainers for checking context

--- book-tracker-service/src/test/java/com/example/booktrackerservice/BookTrackerServiceApplicationTests.java
+++ book-tracker-service/src/test/java/com/example/booktrackerservice/BookTrackerServiceApplicationTests.java
@@ -2,12 +2,34 @@ package com.example.booktrackerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class BookTrackerServiceApplicationTests {
+@Testcontainers
+public class BookTrackerServiceApplicationTests {
+
+	@Container
+	private static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8"))
+			.withDatabaseName("testdb")
+			.withUsername("testuser")
+			.withPassword("testpass");
+
+	@DynamicPropertySource
+	static void setDataSourceProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", mysqlContainer::getUsername);
+		registry.add("spring.datasource.password", mysqlContainer::getPassword);
+		registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+	}
 
 	@Test
 	void contextLoads() {
+		assertTrue(true); 
 	}
-
 }
